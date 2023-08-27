a7091b700e753bc0942d1cafa3bd88661881e76f
EricTruchot <eric.truchot12@gmail.com>
2023-08-27 19:32:31 +0200

    install dependencies, react-native-maps

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MapPage from './App/Map/MapPage';
 
 function HomeScreen() {
   return (
@@ -11,13 +12,6 @@ function HomeScreen() {
   );
 }
 
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
 
 const Tab = createBottomTabNavigator();
 
@@ -26,7 +20,7 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="MapPage" component={MapPage} />
       </Tab.Navigator>
     </NavigationContainer>
   );
