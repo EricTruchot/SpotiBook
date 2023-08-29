c0d5fe128bbc6f274da599068f3077ed4143cea7
gharioss <njulie818@gmail.com>
2023-08-29 22:25:22 +0200

    feat(map): afficher les markers sur la map en fonction des données.

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState, useRef } from "react";
+import { StyleSheet } from "react-native";
+import MapView, { Marker } from 'react-native-maps';
 
-export default function Map() {
+export default function Map({ markers }) {
+  const [latitude, setLatitude] = useState(43.52252062905719);
+  const [longitude, setLongitude] = useState(5.449523280048378);
+
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+      mapRef.current?.animateToRegion({
+        latitude: latitude,
+        longitude: longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      })
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <MapView style={styles.map} />
-    </View>
+      <MapView
+        ref={mapRef}
+        userLocationPriority="high"
+        showsUserLocation={true}
+        style={styles.map}
+        >
+        { Object.keys(markers)?.length > 0  && markers?.map(marker => (
+            <Marker
+            key={marker?.id}
+            coordinate={{latitude: marker?.latitude, longitude: marker?.longitude}}
+            title={marker?.nom}
+        />
+        ))}
+        
+      </MapView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
   map: {
     width: '100%',
     height: '100%',
   },
-});
+})
