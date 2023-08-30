ca4f5055d0fb06519759e8de36f5ececae9f6cf4
gharioss <njulie818@gmail.com>
2023-08-30 22:52:11 +0200

    feat(functions): création des fonctionnalités de login / logout / getUserById / getAllBoxes

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
+import React, { useState, useEffect } from 'react';
 import AccueilUnregistered from "./src/app/accueil/AccueilUnregistered";
 import Map from "./src/app/map/MapPage";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AccueilRegistered from "./src/app/accueil/AccueilRegistered";
+import { retrieveData } from './src/app/services/localStorageUsers';
 
 export default function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const Tab = createBottomTabNavigator();
-
     return (
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name="Accueil" component={ AccueilUnregistered } />
-                <Tab.Screen name="AccueilRegistered" component={ AccueilRegistered } />
+                
+                { retrieveData('isLoggedIn') ? (
+                    <Tab.Screen name="Accueil" component={ AccueilUnregistered } />
+                ) : (
+                    <Tab.Screen name="AccueilRegistered" component={ AccueilRegistered } />
+                )}
+                
                 <Tab.Screen name="Carte" component={ Map } />
+
+                { !retrieveData('isLoggedIn') && (
+                    <Tab.Screen name="Carte2" component={ Map } />
+                )}
             </Tab.Navigator>
         </NavigationContainer>
     )
--- src/app/accueil/AccueilUnregistered.js
+++ src/app/accueil/AccueilUnregistered.js
@@ -1,16 +1,43 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, Button } from 'react-native';
 import { StyleSheet } from "react-native";
+import { retrieveData, storeData, removeData } from '../services/localStorageUsers.js';
+import { getUserById } from '../services/User';
 
 export default function AccueilUnregistered() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [idk, setIdk] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+        await getAllSpots();
+      })();
+    }, []);
+
+    async function getAllSpots() {
+        const allSpots = await getUserById("03b04b8e-1b73-4954-a4d8-fb76a6feda19");
+        setIdk(allSpots)
+    }
+
+    console.log(idk);
+
   return (
     <>
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Page d'accueil</Text>
       <Button
+        onPress={() => {storeData('isLoggedIn', 'yes'), setIsLoggedIn(true)}}
         title="Connexion"
         color="#841584"
         />
+
+      <Button
+        onPress={() => {removeData('isLoggedIn'), setIsLoggedIn(false)}}
+        title="Déco"
+        color="#841584"
+        />
+
+        {isLoggedIn ? <Text>Hello</Text> : <Text>No</Text>}
     </View>
     </>
   );
