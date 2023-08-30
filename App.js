928fc224941e6325c90795da0013b7fe9007ad73
gharioss <njulie818@gmail.com>
2023-08-30 22:52:11 +0200

    feat(router): création barcode + router dynamique. Mis en place d'un barcode + d'un route dynamique qui change la bottomBar

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import AccueilUnregistered from "./src/app/accueil/AccueilUnregistered";
 import Map from "./src/app/map/MapPage";
 import { NavigationContainer } from "@react-navigation/native";
@@ -5,12 +6,16 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AccueilRegistered from "./src/app/accueil/AccueilRegistered";
 
 export default function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const Tab = createBottomTabNavigator();
 
+    console.log("isLoggedin :D == ", isLoggedIn);
     return (
         <NavigationContainer>
             <Tab.Navigator>
-                <Tab.Screen name="Accueil" component={ AccueilUnregistered } />
+                <Tab.Screen name="Accueil">
+                    {() => <AccueilUnregistered loggedIn={ isLoggedIn } setLoggedIn={ setIsLoggedIn } />}
+                </Tab.Screen>
                 <Tab.Screen name="AccueilRegistered" component={ AccueilRegistered } />
                 <Tab.Screen name="Carte" component={ Map } />
             </Tab.Navigator>
--- src/app/accueil/AccueilUnregistered.js
+++ src/app/accueil/AccueilUnregistered.js
@@ -1,15 +1,51 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View, Button } from 'react-native';
 import { StyleSheet } from "react-native";
+import { storeData, removeData } from '../services/localStorageUsers.js';
+import { getUserById } from '../services/api.js';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
-export default function AccueilUnregistered() {
+export default function AccueilUnregistered({ loggedIn, setLoggedIn }) {
+  const [scanned, setScanned] = useState(false);
+  const [hasPermission, setHasPermission] = useState(null);
+
+    useEffect(() => {
+      const getBarCodeScannerPermissions = async () => {
+        const { status } = await BarCodeScanner.requestPermissionsAsync();
+        setHasPermission(status === 'granted');
+      };
+  
+      getBarCodeScannerPermissions();
+    }, []);
+
+    const handleBarCodeScanned = ({ type, data }) => {
+      setScanned(true);
+      ( async () => {
+        // let result = await ResultsFetchAuthentification(data);
+        // console.log(data)
+      })();
+  
+    };
+
   return (
     <>
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Page d'accueil</Text>
       <Button
+        onPress={() => {storeData('isLoggedIn', 'yes'), setLoggedIn(true)}}
         title="Connexion"
         color="#841584"
+        />
+
+      <Button
+        onPress={() => {removeData('isLoggedIn'), setLoggedIn(false)}}
+        title="Déco"
+        color="#841584"
+        />
+
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={{ height: 400, width: 400 }}
         />
     </View>
     </>
@@ -22,3 +58,16 @@ const styles = StyleSheet.create({
     height: '100%',
   },
 })
+
+
+
+  // useEffect(() => {
+  //   (async () => {
+  //       await getAllSpots();
+  //     })();
+  //   }, []);
+
+  //   async function getAllSpots() {
+  //       const allSpots = await getUserById("03b04b8e-1b73-4954-a4d8-fb76a6feda19");
+  //       setIdk(allSpots)
+  //   }
